e2f7c49140fb7991f94e53e1ee032f2ef29b3824
frostymuaddib <frosty87@gmail.com>
2023-06-27 11:43:46 +0200

    Implemented ModifyOffer method, added descriptions to the methods of Trading contract. Optimized some methods

--- market.sol
+++ market.sol
@@ -6,7 +6,7 @@ import "./tokenDispenser.sol";
 struct TradeOffer {
     address payable sellerAddress;
     uint256 energyAmount;
-    uint validUntil; //timestamp do kada vazi.
+    uint validUntil; //unix timestamp 
     uint pricePerEnergyAmount; //in Wei
     bool exists; //flag for reading from mapping
 }
@@ -22,7 +22,7 @@ contract Trading {
     //event that fires when an offer is created
     event OfferCreated(uint256 id, address seller, uint validUntil, uint pricePerEnergyAmount, uint256 energyAmount);
 
-    //event that fires when some of the electricity
+    //event that fires when offer is modified
     event OfferModified(uint256 id, address seller, uint validUntil, uint pricePerEnergyAmount, uint256 energyAmount);
     
     //event that fires when offer is closed (sold or expired)
@@ -40,19 +40,22 @@ contract Trading {
         owner = msg.sender;
     }
 
-    // a seller creates an offer, price is in Wei
+    /* Method used by energy seller to create an offer. Fires OfferCreated event.
+    
+    NOTE: Before creating an offer, for security reasons, the seller must
+    authorize this smart contract to transfer tokens in seller's name using increaseAllowance method of ERC20 token: 
+    increaseAllowance(contractAddress, energyAmount).
+
+    Params:
+    validUntil - Unix timestamp that marks the time when the offer ends
+    pricePerEnergyAmount - price, in Wei, for every unit of energy being sold
+    energyAmount - the amount of energy (tokens) for sell
+    */
     function CreateEnergyOffer(uint validUntil, uint pricePerEnergyAmount, uint256 energyAmount) public {
         require(ep.IsRegistered(msg.sender), "Only registered user (meter) may make sell offers");
         require(energyAmount <= ep.balanceOf(msg.sender), "Must have tokens to sell.");
         require(validUntil > block.timestamp,"Offer deadline must be in the future.");//TODO: videti koliko u buducnosti, jer block.timestamp nije bas trenutno vreme
-        //iz bezbednosnih razloga, korisnik mora da pozove approve i da dozvoli da ovaj ugovor radi sa energyAmount tokena koje prebaci sebi na adresu
-        //ovo mora da radi eksplicitno da bi sledece radilo!
-        require(energyAmount <= ep.allowance(msg.sender, address(this)), "User made no allowance to contract");
         ep.transferFrom(msg.sender, address(this), energyAmount);
-        //ep.transfer(address(this), energyAmount); //prebaci tokene na ugovor
-        //bytes memory data = abi.encodeWithSignature("_transfer(address, uint256)", msg.sender, energyAmount);
-
-        //(bool success, ) =  epAddress.call(data);
         uint256 id = getID();
 
         offers[id] = TradeOffer({
@@ -64,7 +67,15 @@ contract Trading {
         emit OfferCreated(id, msg.sender, validUntil, pricePerEnergyAmount, energyAmount);
     }
 
-    //price is in Wei
+    /* Method used by energy buyers to buy enegry from an offer. The buyer doesn't need to buy all energy from the offer. Fires OfferModified or OfferClosed events.
+    When method is closed, seller may recover remaining unsold tokens using RetrieveTokens method.
+    
+    NOTE: Method is payable, which means that buyer must send at least energyAmount*offers[offerId].pricePerEnergyAmount Weis when calling this method.
+
+    Params:
+    offerId - unique id of the offer
+    energyAmount - the amount of energy (tokens) buyer is buying
+    */
     function BuyEnergyFromOffer(uint256 offerId, uint256 energyAmount) public payable {
         require(ep.IsRegistered(msg.sender), "Only registered user (meter) may buy energy from offers");
         require(offers[offerId].exists , "No offer with given ID." );
@@ -73,13 +84,7 @@ contract Trading {
         require(msg.value >= energyAmount*offers[offerId].pricePerEnergyAmount, "Not enough money sent to buy energy");
         //send money to seller
         offers[offerId].sellerAddress.transfer(msg.value);
-        //update offer
         offers[offerId].energyAmount -= energyAmount;
-        //prebaci tokene kupcu sa ugovora
-        //ep._transfer(address(this), msg.sender, energyAmount );
-        //bytes memory data = abi.encodeWithSignature("_transfer(address, uint256)", msg.sender, energyAmount);
-
-        //(bool success, ) =  epAddress.call(data);
         ep.transfer(msg.sender, energyAmount);
         
         if(offers[offerId].energyAmount == 0)
@@ -88,12 +93,42 @@ contract Trading {
             emit OfferModified(offerId, offers[offerId].sellerAddress, offers[offerId].validUntil, offers[offerId].pricePerEnergyAmount, offers[offerId].energyAmount);
     }
 
+    /* Method used by energy selelrs to modify their existing offers. Fires OfferModified event.
+       
+    NOTE: Before modifying an offer, for security reasons, the seller must
+    authorize this smart contract to transfer tokens in seller's name using increaseAllowance method of ERC20 token accordingly.
+
+    Params:
+    offerId - unique id of the offer
+    validUntil - new Unix timestamp that marks the time when the offer ends
+    pricePerEnergyAmount - new price, in Wei, for every unit of energy being sold
+    energyAmount - new amount of energy (tokens) for sell
+    */
+    function ModifyOffer(uint256 offerId, uint validUntil, uint pricePerEnergyAmount, uint256 energyAmount) public {
+        //require(ep.IsRegistered(msg.sender), "Only registered user (meter) may buy energy from offers");
+        require(msg.sender == offers[offerId].sellerAddress, "Only creator of the offer can modify it."); //implicitly checks if caller is registered
+        require(offers[offerId].validUntil>= block.timestamp, "The offer must be active.");
+        require(validUntil > block.timestamp,"Offer deadline must be in the future.");
+        require(energyAmount < offers[offerId].energyAmount || energyAmount <= offers[offerId].energyAmount + ep.balanceOf(msg.sender), "Invalid energy amount");
+        require(energyAmount <= ep.allowance(msg.sender, address(this)), "Not enough allowance");
+        offers[offerId].energyAmount = energyAmount;
+        offers[offerId].validUntil = validUntil;
+        offers[offerId].pricePerEnergyAmount = pricePerEnergyAmount;
+        emit OfferCreated(offerId, offers[offerId].sellerAddress , offers[offerId].validUntil, offers[offerId].pricePerEnergyAmount, offers[offerId].energyAmount);
+    }
+
+    /* Method used by energy selelrs to retrieve their tokens from the smart contract after the offer expires.
+       
+    NOTE: Tokens can be recovered only from expired offers.
+
+    Params:
+    offerId - unique id of the offer
+    */
     function RetrieveTokens(uint256 offerId) public {
         require(offers[offerId].exists , "No offer with given ID." );
         require(msg.sender == offers[offerId].sellerAddress, "Only creator of the offer can retrieve tokens."); //implicitly checks if caller is registered
         require(offers[offerId].validUntil< block.timestamp, "The offer is still active.");
         require(offers[offerId].energyAmount>0, "No tokens the retrieve, the offer was already closed.");
-        //ep._transfer(address(this), msg.sender, offers[offerId].energyAmount );
         ep.transfer(msg.sender, offers[offerId].energyAmount );
         offers[offerId].energyAmount = 0;
 
