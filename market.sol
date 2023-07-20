423e39587181a48e4b7da967f90f1ef61ffac400
mknezevic <knezevic.milica@gmail.com>
2023-07-20 10:41:24 +0200

    rename tokenDispenser.sol

--- market.sol
+++ market.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import "./tokenDispenser.sol";
+import "./TokenDispenser.sol";
 
 struct TradeOffer {
     address payable sellerAddress;
